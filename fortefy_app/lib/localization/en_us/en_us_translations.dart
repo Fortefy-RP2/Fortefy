d3a1dd1fef1ed8fd808eb600d7990aa72a1d4453
arturd05 <artur.inazaki@usp.br>
2024-10-02 18:20:24 -0300

    Adição pasta localization e arquivos utils

---
+++ fortefy_app/lib/localization/en_us/en_us_translations.dart
@@ -0,0 +1,84 @@
+final Map<String, String> enUs = {
+
+  "lbl_agendamento": "Agendamento",
+  "lbl_alterar": "Alterar",
+  "lbl_pagamento": "Pagamento",
+  "lbl_pagar": "Pagar",
+  "lbl_seu_personal": "Seu personal",
+  "lbl_veja_mais": "Veja mais",
+  "msg_confirmar_agendamento": "Confirmar agendamento",
+  "msg_data":
+      "Data..........................................................03/10/2024",
+  "msg_lembrando_que_ser":
+      "Lembrando que será cobrada uma taxa de 10% do valor\napenas para o primeiro treino. As demais aulas não serão cobradas",
+  "msg_lembrando_que_ser2":
+      "Lembrando que será cobrada uma taxa de 10% do valor\napenas para o primeiro treino. As demais aulas ",
+  "msg_n_o_ser_o_cobradas": "não serão cobradas!",
+  "msg_qtd_de_horas":
+      "Qtd de horas .................................................. 2horas",
+  "msg_subtotal":
+      "Subtotal.................................................. R\$1.120,00",
+  "msg_total":
+      "Total..................................................... R\$1.232,00",
+
+
+  "lbl_4_5_150": "4.5 (150+)",
+  "lbl_4_9_100": "4.9 (100+)",
+  "lbl_agendar": "Agendar",
+  "lbl_r_300_p_hora": "R\$300 p/hora",
+  "lbl_r_400_p_hora": "R\$400 p/hora",
+  "lbl_rodrigo_g_oes": "Rodrigo Góes",
+  "lbl_zeka_ramos": "Zeka Ramos",
+  "msg_articula_es_treino": "Articulações, treino natural, \nfortalecimento",
+  "msg_distrito_federal:" "Distrito Federal, Brasília",
+
+
+  "lbl_aluno" : "Aluno", "lbl_personal": "Personal", "lbl_voc": "Você é...",
+
+
+  "lbl_entrar": "Entrar",
+  "msg_continuar_com_o": "Continuar com o Google",
+  "msg_continuar_com_o2": "Continuar com o Facebook",
+  "msg_n_o_tem_cadastro": "Não tem cadastro? Clique aqui.",
+
+
+  "msg_voc_ainda_n_o_tem":
+      "Você ainda não tem treinos ativos. Pesquise por um personal",
+
+
+  "lbl_buscar": "Buscar",
+  "lbl_feminino": "Feminino",
+  "lbl_masculino": "Masculino",
+  "lbl_outros": "Outros",
+  "lbl_r_500": "R\$500",
+  "lbl_voltar": "Voltar",
+  "msg_selecione_a_regi_o": "Selecione a região onde deseja um personal:",
+  "msg_selecione_o_sexo": "Selecione o sexo:",
+  "msg_selecione_o_tipo": "Selecione o tipo de serviço:",
+  "msg_selecione_o_valor": "Selecione o valor máximo desejado:",
+
+
+  "msg_voc_personal": "Você é personal? Clique aqui.",
+
+
+  "lbl_cref": "CREF:", "msg_voc_aluno_clique": "Você é aluno? Clique aqui.",
+
+
+  "lbl_5_0_300": "5.0 (300+)",
+  "lbl_cadastrar": "Cadastrar",
+  "lbl_email": "Email:",
+  "lbl_nome_completo": "Nome completo:",
+  "lbl_ou": "Ou",
+  "lbl_r_560_p_hora": "R\$560 p/hora",
+  "lbl_renato_cariani": "Renato Cariani",
+  "lbl_senha": "Senha",
+  "msg_data_de_nascimento": "Data de nascimento",
+  "msg_escreva_o_servi_o": "Escreva o serviço desejado:",
+  "msg_fisiculturismo": "Fisiculturismo, musculação, \nemagrecimento",
+  "msg_repita_sua_senha": "Repita sua senha:",
+  "msg_s_o_paulo_s_o_caetano": "São Paulo, São Caetano do Sul.",
+
+
+  "msg_network_err": "Network Error",
+  "msg_something_went_wrong": "Something Went Wrong!",
+};
